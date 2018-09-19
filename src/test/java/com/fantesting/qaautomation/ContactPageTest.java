52f1a4768611db7a302128c897552988812acb46
Farhad Ali <farhad.ali@paaysay.co.uk>
2018-09-19 09:33:08 +0100

    farhad ali contact page class

---
+++ src/test/java/com/fantesting/qaautomation/ContactPageTest.java
@@ -0,0 +1,22 @@
+package com.fantesting.qaautomation;
+
+import com.fantesting.qaautomation.pages.AboutPage;
+import com.fantesting.qaautomation.pages.ContactPage;
+import com.fantesting.qaautomation.pages.HomePage;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ContactPageTest extends BaseTest {
+
+    @Test
+    public void testNumberOfOfficesAreCorrect(){
+        HomePage homePage = navigateToValtechWebsite();
+       AboutPage aboutPage = homePage.navigateToAbout();
+       ContactPage contactPage=aboutPage.navigateToContactPage();
+       int officeCount = contactPage.numberOfOffices();
+        Assert.assertEquals(officeCount,37);
+
+
+
+    }
+}
---
+++ src/test/java/com/fantesting/qaautomation/pages/ContactPage.java
@@ -0,0 +1,27 @@
+package com.fantesting.qaautomation.pages;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import java.util.List;
+
+
+public class ContactPage {
+
+
+    private WebDriver driver;
+
+
+    public ContactPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public int numberOfOffices() {
+        List<WebElement> officeList = driver.findElements(By.xpath("//li[@class='office']"));
+        return officeList.size();
+    }
+
+
+}
