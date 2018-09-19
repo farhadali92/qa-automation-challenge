7457a11923596412622476b785bb970fc4f62a5c
Farhad Ali <farhad.ali@paaysay.co.uk>
2018-09-19 09:28:21 +0100

    farhad ali contact page class

--- src/test/java/com/fantesting/qaautomation/pages/AboutPage.java
+++ src/test/java/com/fantesting/qaautomation/pages/AboutPage.java
@@ -2,15 +2,22 @@ package com.fantesting.qaautomation.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
+
 public class AboutPage {
 
     @FindBy(xpath = ".//div[@class='page-header']/h1")
     private WebElement pageHeader;
+
+    @FindBy(xpath = "//a[@href ='/about/contact-us/']")
+    private WebElement ourOfficesLink;
+
 
     private WebDriver driver;
 
@@ -24,5 +31,11 @@ public class AboutPage {
 
     public String getHeaderText() {
         return pageHeader.getText();
+    }
+
+    public ContactPage  navigateToContactPage(){
+        Actions actions = new Actions(driver);
+        actions.moveToElement(ourOfficesLink).click().build().perform();
+        return new ContactPage(driver);
     }
 }
